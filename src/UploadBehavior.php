51026c6781ba13413396e691f8a3237a9b6f247d
hetao <haohetao@gmail.com>
2020-04-30 17:37:15 +0800

    fix upload file when no file

--- src/UploadBehavior.php
+++ src/UploadBehavior.php
@@ -544,41 +544,44 @@ class UploadBehavior extends \yii\base\Behavior
         }
         if (is_object($file)) {
             $files = [$file];
-        } else {
+        } else if (is_array($file)) {
             $files = $file;
         }
-        foreach ($files as $one) {
-            if ($one && $one->hasError) {
-                switch ($one->error) {
-                    case UPLOAD_ERR_INI_SIZE:
-                        $message = "The uploaded file exceeds the upload_max_filesize directive in php.ini";
-                        break;
-                    case UPLOAD_ERR_FORM_SIZE:
-                        $message = "The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form";
-                        break;
-                    case UPLOAD_ERR_PARTIAL:
-                        $message = "The uploaded file was only partially uploaded";
-                        break;
-                    case UPLOAD_ERR_NO_FILE:
-                        $message = "No file was uploaded";
-                        break;
-                    case UPLOAD_ERR_NO_TMP_DIR:
-                        $message = "Missing a temporary folder";
-                        break;
-                    case UPLOAD_ERR_CANT_WRITE:
-                        $message = "Failed to write file to disk";
-                        break;
-                    case UPLOAD_ERR_EXTENSION:
-                        $message = "File upload stopped by extension";
-                        break;
+        if ($files) {
+            foreach ($files as $one) {
+                if ($one && $one->hasError) {
+                    switch ($one->error) {
+                        case UPLOAD_ERR_INI_SIZE:
+                            $message = "The uploaded file exceeds the upload_max_filesize directive in php.ini";
+                            break;
+                        case UPLOAD_ERR_FORM_SIZE:
+                            $message = "The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form";
+                            break;
+                        case UPLOAD_ERR_PARTIAL:
+                            $message = "The uploaded file was only partially uploaded";
+                            break;
+                        case UPLOAD_ERR_NO_FILE:
+                            $message = "No file was uploaded";
+                            break;
+                        case UPLOAD_ERR_NO_TMP_DIR:
+                            $message = "Missing a temporary folder";
+                            break;
+                        case UPLOAD_ERR_CANT_WRITE:
+                            $message = "Failed to write file to disk";
+                            break;
+                        case UPLOAD_ERR_EXTENSION:
+                            $message = "File upload stopped by extension";
+                            break;
 
-                    default:
-                        $message = "Unknown upload error";
-                        break;
+                        default:
+                            $message = "Unknown upload error";
+                            break;
+                    }
+                    throw new ErrorException($message);
                 }
-                throw new ErrorException($message);
             }
         }
+
         return $file;
     }
 
